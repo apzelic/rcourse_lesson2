30cdfbba00652913473a1cd44646ae4da55ded0a
Alan Zelicoff <zalan8587@q.com>
2017-04-03 22:48:44 -0600

    Commiting statistics script

---
+++ scripts/rcourse_lesson2_statistics.R
@@ -0,0 +1,28 @@
+## READ IN DATA ####
+source("scripts/rcourse_lesson2_cleaning.R")
+
+## LOAD PACKAGES ####
+# [none currently needed]
+
+## ORGANIZE DATA for statistical calculations ####
+data_stats = data_clean
+
+## BUILD MODEL - PROPORTION OF 'PAGE'S BY YEAR (CONTINUOUS PREDICTOR) ####
+year.lm <- lm(prop_log10 ~ year, data = data_stats)
+
+year.lm_sum = summar(year.lm)
+year.lm_sum
+
+year.lm_resid = resid(year.lm)
+
+
+## BUILD MODEL - PROPORTION OF 'PAGE'S BY SEX (CATEGORICAL PREDICTOR) ####
+sex.lm = lm(prop_log10 ~ sex, data = data_stats)
+
+sex.lm_sum = summary(sex.lm)
+sex.lm_sum
+
+sex.lm_resid = resid(sex.lm)
+head(sex.lm_resid)
+
+
